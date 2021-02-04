1c6925fdb030215f1f904c3a065a956c808acc91
CGantert345 <57003061+CGantert345@users.noreply.github.com>
2021-02-04 13:23:48 +0100

    distance price calculation fixed

--- GTM data converter.editor/src/Gtm/actions/converter/ConverterFromLegacy.java
+++ GTM data converter.editor/src/Gtm/actions/converter/ConverterFromLegacy.java
@@ -1616,6 +1616,7 @@ public class ConverterFromLegacy {
 			}
 		}  else {
 			float price = 0;
+			boolean found = false;
 			
 			int distance = 0;
 			if (travelClass == 1) {
@@ -1627,9 +1628,7 @@ public class ConverterFromLegacy {
 				//distance = 0 indicates no price in that class!
 				if (distance == 0) return null;
 			}
-			
-			
-					
+							
 			//get the lowest price where the distance is ok
 			for (LegacyDistanceFare fare : tool.getConversionFromLegacy().getLegacy108().getLegacyDistanceFares().getDistanceFare()) {
 				if ( (     fare.getValidFrom().before(dateRange.getStartDate())
@@ -1638,12 +1637,14 @@ public class ConverterFromLegacy {
 					   ||fare.getValidUntil().equals(dateRange.getEndDate()) ) )  {
 				
 					if (travelClass == 1) {
-						if (distance >= fare.getDistance() && fare.getFare1st() > price) {
+						if (distance <= fare.getDistance() && (fare.getFare1st() < price || !found)) {
 							price = fare.getFare1st();
+							found = true;
 						}
 					} else {
-						if (distance >= fare.getDistance() && fare.getFare2nd() > price) {
+						if (distance <= fare.getDistance() && (fare.getFare2nd() < price || !found)) {
 							price = fare.getFare2nd();
+							found = true;
 						}				
 					}
 				}
