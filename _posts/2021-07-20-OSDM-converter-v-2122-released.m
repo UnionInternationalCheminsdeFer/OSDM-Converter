ee7bb715dcf91e2c9850fd550ccc46651d434da8
CGantert345 <57003061+CGantert345@users.noreply.github.com>
2021-07-20 13:46:03 +0200

    Create 2021-07-20-OSDM-converter-v-2122-released.m

---
+++ _posts/2021-07-20-OSDM-converter-v-2122-released.m
@@ -0,0 +1,14 @@
+---
+layout: post
+title: "OSDM Converter V.2.1.22 Released"
+author: Clemens Gantert
+date: 2021-07-22 11:00:33 +0100
+categories: osdm update
+---
+
+Converter version 2.1.22
+
+Bug Fixes on complex routes
+ - alternative routes with more than one station per route (OSDM to 108)
+ - multiple alternative routes A/B * C/D (108 to OSDM)
+ - error message in case of more than three alternative routes (A/B/C) (OSDM to 108)
